63d7d938ffeb78e3f32d6fc47bf8b3331b35363a
Ismail Useinov <ismail_2016@mail.ru>
2023-11-13 13:22:59 +0300

    lesson 3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Счётчик
 const Counter = {
   data() {
     return {
@@ -11,7 +12,7 @@ const Counter = {
   }
 }
 Vue.createApp(Counter).mount('#counter')
-
+// Директива v-bind
 const AttributeBinding = {
   data() {
     return {
@@ -20,3 +21,29 @@ const AttributeBinding = {
   }
 }
 Vue.createApp(AttributeBinding).mount('#bind-attribute')
+// Директива v-on
+const EventHandling = {
+  data() {
+    return {
+      message: 'Привет, Vue.js!'
+    }
+  },
+  methods: {
+    reverseMessage() {
+      this.message = this.message
+        .split('')
+        .reverse()
+        .join('')
+    }
+  }
+}
+Vue.createApp(EventHandling).mount('#event-handling')
+// Директива v-model
+const TwoWayBinding = {
+  data() {
+    return {
+      message: 'Привет, World!'
+    }
+  }
+}
+Vue.createApp(TwoWayBinding).mount('#two-way-binding')
